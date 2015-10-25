f733fcb8039e9416280a5f279ec282486c0c0b3c
Luminarys <kizunanohikari@gmail.com>
2015-10-24 22:05:28 -0500

    Switched to /tmp/ files, changed name of current-window-ss.sh to window-ss.sh

--- desktop-ss.sh
+++ desktop-ss.sh
@@ -2,7 +2,7 @@
 # Author: Luminarys
 # Requires: bash, maim, curl, jq
 # Captures your desktop, uploads it to fuwa, then copies the URL to your clipboard and opens the picture in your browser.
-FILE=/home/$(whoami)/Pictures/Screenshots/$(date +%s)-maim.png
+FILE=/tmp/$(date +%s)-maim.png
 maim "$FILE"
 URL=$(curl -F "file=@${FILE}" https://fuwa.se/api/upload | jq -r '.url')
 echo "$URL" | xsel -p
--- window-ss.sh
+++ window-ss.sh
@@ -2,7 +2,7 @@
 # Author: Luminarys
 # Requires: bash, slop, maim, curl, jq
 # Captures the specified window, uploads it to fuwa, then copies the URL to your clipboard and opens the picture in your browser.
-FILE=/home/$(whoami)/Pictures/Screenshots/$(date +%s)-maim.png
+FILE=/tmp/$(date +%s)-maim.png
 maim -s -p 10 "$FILE"
 URL=$(curl -F "file=@${FILE}" https://fuwa.se/api/upload | jq -r '.url')
 echo "$URL" | xsel -p
